2563868295230dc403007d22d0f8c66b04706bb1
saketh169 <147064850+saketh169@users.noreply.github.com>
2025-09-17 16:13:56 +0530

    main

---
+++ my-react-app/src/Students.jsx
@@ -0,0 +1,11 @@
+
+const students = [
+  { key:1, name: "Saketh Pabbu", rollNumber: 169, marks: 96 },
+  { key:2 ,name: "Sriram", rollNumber: 172, marks: 89 },
+  { key:3 , name: "Pradeep", rollNumber: 165, marks: 78},
+  { key:4 ,name: "Harshil", rollNumber: 146, marks: 64 },
+   { key:5 , name: "Shoban", rollNumber: 152, marks: 100 },
+    { key:6 ,name: "Vineeth", rollNumber: 158, marks: 99 }
+];
+
+export default students;
